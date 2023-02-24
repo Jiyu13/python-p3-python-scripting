38f10beb98582cf8c468a8f34581bd34556d4ba7
Ziru Zou <jiyucoding@yahoo.com>
2023-02-24 04:39:09 -0500

    code along

--- bin/script.py
+++ bin/script.py
@@ -2,3 +2,17 @@
 import os
 if __name__ == "__main__":
     os.system('ls -l')
+
+
+
+# can run script.py using './script.py' instead of 'python script.py'
+
+# ./bin/script.py
+# return:
+# total 28
+# -rw-r--r-- 1 ziru ziru 1810 Feb 24 03:12 CONTRIBUTING.md
+# -rw-r--r-- 1 ziru ziru 1346 Feb 24 03:12 LICENSE.md
+# -rw-r--r-- 1 ziru ziru  141 Feb 24 03:12 Pipfile
+# -rw-r--r-- 1 ziru ziru  456 Feb 24 03:12 Pipfile.lock
+# -rw-r--r-- 1 ziru ziru 4858 Feb 24 03:12 README.md
+# drwxr-xr-x 2 ziru ziru 4096 Feb 24 04:32 bin
---
+++ bin/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Files in this directory"
+ls -a
+
+# give it excutable permission before runing the script
+# chmod +x ./bin/script.sh
+#  run "ls"
+# => CONTRIBUTING.md  LICENSE.md  Pipfile  Pipfile.lock  README.md  bin
